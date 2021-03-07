1f247ba264f405d37e97d3f5c5ae042d914e493a
LuximusHunter <49814847+luximus-hunter@users.noreply.github.com>
2021-03-07 21:52:31 +0100

    format

--- src/parts/Footer/index.tsx
+++ src/parts/Footer/index.tsx
@@ -1,7 +1,7 @@
 import './style.scss';
 
 export default () => (
-  <footer id="links">
+  <footer id='links'>
     <div>
       <a href='https://github.com/luximus-hunter'>
         <i className='fab fa-github-square'></i>
